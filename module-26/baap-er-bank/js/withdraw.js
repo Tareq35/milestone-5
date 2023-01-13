cd559acd138e002224c428101875e7e4b0f67639
Tareq35 <a.t.tariqulislam@gmail.com>
2023-01-14 02:40:54 +0600

    code updated

--- module-26/baap-er-bank/js/withdraw.js
+++ module-26/baap-er-bank/js/withdraw.js
@@ -4,18 +4,34 @@ document.getElementById('btn-withdraw').addEventListener('click', function () {
     const withdrawField = document.getElementById('withdraw-field');
     const newWithdrawAmount = parseFloat(withdrawField.value);
 
+    // last step: clear the withdraw field
+    withdrawField.value = '';
+
+    if (isNaN(newWithdrawAmount)) {
+        alert('Please provide a valid number');
+        return;
+    }
+
+
     // step-3: get the current withdraw total
     const withdrawTotalElement = document.getElementById('withdraw-total');
     const previousWithdrawTotal = parseFloat(withdrawTotalElement.innerText);
+
+    // step-5: get the total balance;
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotal = parseFloat(balanceTotalElement.innerText);
+
+
+
+    if (newWithdrawAmount > previousBalanceTotal) {
+        alert('Baap er bank a ato taka nai');
+        return;
+    }
 
     // step-4: add numbers to set the total withdraw
     const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
 
     withdrawTotalElement.innerText = currentWithdrawTotal;
-
-    // step-5: get the total balance;
-    const balanceTotalElement = document.getElementById('balance-total');
-    const previousBalanceTotal = parseFloat(balanceTotalElement.innerText);
 
     // step-6: calculate current total balance
     const currentBalanceTotal = previousBalanceTotal - newWithdrawAmount;
