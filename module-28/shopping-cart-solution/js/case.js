04d917acc834f158ae9a3db6f46575d33f22f5f1
Tareq35 <a.t.tariqulislam@gmail.com>
2023-01-17 01:25:53 +0600

    code updated

---
+++ module-28/shopping-cart-solution/js/case.js
@@ -0,0 +1,33 @@
+function updateCaseNumber(isIncrease) {
+    const caseNumberField = document.getElementById('case-number-field');
+    const previousCaseNumber = parseInt(caseNumberField.value);
+
+    let newCaseNumber;
+
+    if (isIncrease === true) {
+        newCaseNumber = previousCaseNumber + 1;
+    }
+    else {
+        newCaseNumber = previousCaseNumber - 1;
+    }
+
+    caseNumberField.value = newCaseNumber;
+
+    return newCaseNumber;
+}
+
+document.getElementById('btn-case-plus').addEventListener('click', function () {
+    const newCaseNumber = updateCaseNumber(true)
+
+    const caseTotalPrice = newCaseNumber * 59;
+    const caseTotalElement = document.getElementById('case-total');
+    caseTotalElement.innerText = caseTotalPrice;
+})
+
+document.getElementById('btn-case-minus').addEventListener('click', function () {
+    const newCaseNumber = updateCaseNumber(false)
+
+    const caseTotalPrice = newCaseNumber * 59;
+    const caseTotalElement = document.getElementById('case-total');
+    caseTotalElement.innerText = caseTotalPrice;
+})
